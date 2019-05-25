7bdbdf6f222f5f8aa3f32b5e63260ce34824e56e
Krzysztof Wojciechowski <ja@krzysztofwojciechowski.pl>
2019-05-25 22:18:00 +0200

    Clean up code and add icon

--- app/src/main/java/pl/krzysztofwojciechowski/ballmaze/java/Constants.java
+++ app/src/main/java/pl/krzysztofwojciechowski/ballmaze/java/Constants.java
@@ -5,4 +5,7 @@ abstract class Constants {
     static final int FLOORS = 25;
     static final float ACCEL_ALPHA = 0.65f;
     static final float ACCEL_SENSITIVITY = 0.03f;
+    static final double CD_RECT_X_DIFF = 2;
+    static final double CD_RECT_Y_DIFF = 1.5;
+    static final double CD_RECT_Y_QUOT = Math.sqrt(2);
 }
--- app/src/main/java/pl/krzysztofwojciechowski/ballmaze/java/GameView.java
+++ app/src/main/java/pl/krzysztofwojciechowski/ballmaze/java/GameView.java
@@ -75,6 +75,38 @@ public class GameView extends SurfaceView implements SurfaceHolder.Callback {
         stopGame();
     }
 
+    private void startGame() {
+        mode = GameMode.INGAME;
+        int range = (int)(screenWidth - (2 * getDim(R.dimen.gap_margin_size)) - gapWidth_px + 1);
+        float ballRangePartial = getDim(R.dimen.ball_range_partial);
+        int partialRange = (int)(range / ballRangePartial);
+        ThreadLocalRandom rand = ThreadLocalRandom.current();
+
+        int[] colorArray = getResources().getIntArray(R.array.floor_colors);
+        heights[0] = (int)getDim(R.dimen.first_floor_height);
+        for (int i = 0; i < Constants.FLOORS; i++) {
+            gaps[i] = (int)getDim(R.dimen.gap_margin_size) + (int)(rand.nextInt(partialRange) * ballRangePartial);
+            colors[i] = colorArray[i % colorArray.length];
+            if (i != 0) heights[i] = heights[i - 1] + (int)getDim(R.dimen.distance_between_floors);
+        }
+
+        maxVisibleFloors = (int)Math.ceil(screenHeight / getDim(R.dimen.distance_between_floors));
+
+        ballX_px = ballCentered_px;
+        ballY_px = 0;
+        topY_px = -ballSize_px;
+        score = 0;
+        thread = new GameThread();
+        thread.start();
+    }
+
+    public void stopGame() {
+        mode = GameMode.START;
+        try {
+            if (thread != null) thread.join();
+        } catch (InterruptedException ignored) {} //NOSONAR
+    }
+
     private void draw() {
         if (!getHolder().getSurface().isValid()) return;
         canvas = getHolder().lockCanvas();
@@ -99,14 +131,11 @@ public class GameView extends SurfaceView implements SurfaceHolder.Callback {
             case WIN:
                 drawWin(canvas);
                 break;
-            default:
-                mode = GameMode.START;
-                drawStart(canvas);
         }
         getHolder().unlockCanvasAndPost(canvas);
     }
 
-    private void drawStart(Canvas canvas) {
+    private void computeDimensions() {
         screenWidth = getWidth();
         screenHeight = getHeight();
         ballCentered_px = screenWidth / 2;
@@ -117,19 +146,24 @@ public class GameView extends SurfaceView implements SurfaceHolder.Callback {
         maxRightBallPosition_px = screenWidth - minLeftBallPosition_px;
         floorRectHeight_px = (int)getDim(R.dimen.floor_rect_height);
         gapWidth_px = (int)getDim(R.dimen.gap_width);
+    }
 
-        // Game title
+    private void drawStart(Canvas canvas) {
+        // Compute dimensions before game starts
+        computeDimensions();
+
+        // Draw game title
         paint.setTextSize(getDim(R.dimen.title_size));
-        drawCenter(canvas, paint, getString(R.string.app_name), -1,
+        drawCenterX(canvas, paint, getString(R.string.app_name),
                 getDim(R.dimen.title_position));
 
-        // Objective: title
+        // Draw objective title
         paint.setTextSize(getDim(R.dimen.objective_title_size));
-        drawCenter(canvas, paint, getString(R.string.objective_title), -1,
+        drawCenterX(canvas, paint, getString(R.string.objective_title),
                 getDim(R.dimen.objective_title_position));
 
-        // Objective text content
-        String objective = getString(R.string.objective);
+        // Draw objective text content
+        String objective = getResources().getString(R.string.objective, Constants.FLOORS);
         TextPaint tp = new TextPaint();
         tp.setTextSize(getDim(R.dimen.objective_text_size));
         tp.setColor(paint.getColor());
@@ -144,19 +178,11 @@ public class GameView extends SurfaceView implements SurfaceHolder.Callback {
         paint.setTextSize(getDim(R.dimen.big_text_size));
         canvas.translate(0, -objPos);
 
-        // START prompt
+        // Draw START prompt
         Typeface oldtf = paint.getTypeface();
         paint.setTypeface(Typeface.DEFAULT_BOLD);
-        drawCenter(canvas, paint, getString(R.string.tap_to_start), -1, -1);
+        drawCenterX(canvas, paint, getString(R.string.tap_to_start), -1);
         paint.setTypeface(oldtf);
-    }
-
-    private float getDim(int resId) {
-        return getResources().getDimension(resId);
-    }
-
-    private String getString(int resId) {
-        return getResources().getString(resId);
     }
 
     private void drawInGame(Canvas canvas) {
@@ -174,7 +200,7 @@ public class GameView extends SurfaceView implements SurfaceHolder.Callback {
         }
 
         // Draw score with shadow
-        paint.setColor(getColor(R.color.textshadow));
+        paint.setColor(getColor(R.color.text_shadow));
         canvas.drawText(
                 Integer.toString(score),
                 getDim(R.dimen.score_margin) + UNIT,
@@ -192,8 +218,16 @@ public class GameView extends SurfaceView implements SurfaceHolder.Callback {
     private void drawAt(int height, Canvas canvas) {
         int top = heights[height] - topY_px;
         paint.setColor(colors[height]);
-        canvas.drawRect(0, top, gaps[height], top + floorRectHeight_px, paint);
-        canvas.drawRect(gaps[height] + gapWidth_px, top, screenWidth, top + floorRectHeight_px, paint);
+        Rect[] rects = getRectsFor(top, gaps[height]);
+        canvas.drawRect(rects[0], paint);
+        canvas.drawRect(rects[1], paint);
+    }
+
+    private Rect[] getRectsFor(int top, int gapDistance) {
+        return new Rect[] {
+            new Rect(0, top, gapDistance, top + floorRectHeight_px),
+            new Rect(gapDistance + gapWidth_px, top, screenWidth, top + floorRectHeight_px)
+        };
     }
 
     private void drawWin(Canvas canvas) {
@@ -219,68 +253,44 @@ public class GameView extends SurfaceView implements SurfaceHolder.Callback {
     private void drawGameOver(Canvas canvas, String wlText) {
         // Game title
         paint.setTextSize(getDim(R.dimen.title_size));
-        drawCenter(canvas, paint, getString(R.string.app_name), -1,
+        drawCenterX(canvas, paint, getString(R.string.app_name),
                 getDim(R.dimen.title_position));
 
         // YOU WON/LOST
         paint.setTextSize(getDim(R.dimen.big_text_size));
-        drawCenter(canvas, paint, wlText, -1,
+        drawCenterX(canvas, paint, wlText,
                 getDim(R.dimen.gameover_title_position));
 
         // Score
-        drawCenter(canvas, paint, getResources().getString(R.string.score, score), -1,
+        drawCenterX(canvas, paint, getResources().getString(R.string.score, score),
                 getDim(R.dimen.gameover_score_position));
 
         // START prompt
         Typeface oldtf = paint.getTypeface();
         paint.setTypeface(Typeface.DEFAULT_BOLD);
-        drawCenter(canvas, paint, getString(R.string.play_again), -1, -1);
+        drawCenterX(canvas, paint, getString(R.string.play_again), -1);
         paint.setTypeface(oldtf);
     }
 
-    private void startGame() {
-        mode = GameMode.INGAME;
-        int range = (int)(screenWidth - (2 * getDim(R.dimen.gap_margin_size)) - gapWidth_px + 1);
-        ThreadLocalRandom rand = ThreadLocalRandom.current();
-
-        int[] colorArray = getResources().getIntArray(R.array.floor_colors);
-        heights[0] = (int)getDim(R.dimen.first_floor_height);
-        for (int i = 0; i < Constants.FLOORS; i++) {
-            gaps[i] = (int)getDim(R.dimen.gap_margin_size) + rand.nextInt(range);
-            colors[i] = colorArray[i % colorArray.length];
-            if (i != 0) heights[i] = heights[i - 1] + (int)getDim(R.dimen.distance_between_floors);
-        }
-
-        maxVisibleFloors = (int)Math.ceil(screenHeight / getDim(R.dimen.distance_between_floors));
-
-        ballX_px = ballCentered_px;
-        ballY_px = 0;
-        topY_px = -ballSize_px;
-        score = 0;
-        thread = new GameThread();
-        thread.start();
-    }
-
-    public void stopGame() {
-        mode = GameMode.START;
-        try {
-            if (thread != null) thread.join();
-        } catch (InterruptedException ignored) {
-            // That should not fail.
-        }
-    }
-
     // https://stackoverflow.com/a/32081250
-    private void drawCenter(Canvas canvas, Paint paint, String text, float x, float y) {
+    private void drawCenterX(Canvas canvas, Paint paint, String text, float y) {
         Rect r = canvas.getClipBounds();
         int cHeight = r.height();
         int cWidth = r.width();
         paint.setTextAlign(Paint.Align.LEFT);
         paint.getTextBounds(text, 0, text.length(), r);
 
-        if (x == -1) x = cWidth / 2f - r.width() / 2f - r.left;
+        float x = cWidth / 2f - r.width() / 2f - r.left;
         if (y == -1) y = cHeight / 2f + r.height() / 2f - r.bottom;
         canvas.drawText(text, x, y, paint);
+    }
+
+    private float getDim(int resId) {
+        return getResources().getDimension(resId);
+    }
+
+    private String getString(int resId) {
+        return getResources().getString(resId);
     }
 
     private int getColor(int colorId) {
@@ -305,7 +315,9 @@ public class GameView extends SurfaceView implements SurfaceHolder.Callback {
                     runGameTick();
                     draw();
                     sleep(16);
-                } catch (InterruptedException ignored) { }
+                } catch (InterruptedException ignored) { // NOSONAR
+                    break;
+                }
             }
         }
 
@@ -316,13 +328,31 @@ public class GameView extends SurfaceView implements SurfaceHolder.Callback {
 
             ballY_px += scrollDist_px;
             topY_px += scrollDist_px;
-            // TODO collision detection
+
+            // Collision detection
+            // The collision will be with the next floor, so floor #score
+            Rect[] rects = getRectsFor(heights[score], gaps[score]);
+            Rect circleRect = getRectForCollisionDetection(ballX_px, ballY_px);
+            if (rects[0].intersect(circleRect) || rects[1].intersect(circleRect)) {
+                mode = GameMode.LOSE;
+            }
+
             if (ballY_px - (2 * ballSize_px) > heights[score]) {
                 score++;
             }
             if (score == Constants.FLOORS) {
                 mode = GameMode.WIN;
             }
+        }
+
+        /** Simplify the ball (circle) into a more forgiving rectangle. */
+        private Rect getRectForCollisionDetection(int circleX, int circleY) {
+            int radius = ballSize_px;
+            int rectLeft = circleX - radius + (int)(Constants.CD_RECT_X_DIFF * UNIT);
+            int rectTop = circleY - (int)(radius / Constants.CD_RECT_Y_QUOT) + (int)(Constants.CD_RECT_Y_DIFF * UNIT);
+            int rectRight = circleX + radius - (int)(Constants.CD_RECT_X_DIFF * UNIT);
+            int rectBottom = circleY + (int)(radius / Constants.CD_RECT_Y_QUOT) - (int)(Constants.CD_RECT_Y_DIFF * UNIT);
+            return new Rect(rectLeft, rectTop, rectRight, rectBottom);
         }
     }
 }
